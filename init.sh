d1d87946b5dc19e44b87c135eb70032d290d3593
Michael.Sun <zzusunwei@126.com>
2019-10-10 21:07:04 +0800

    init

---
+++ init.sh
@@ -0,0 +1,7 @@
+git clone git@github.com:com-datou/eureka-server-example.git
+git clone git@github.com:com-datou/gateway-example.git
+git clone git@github.com:com-datou/monitor-example.git
+git clone git@github.com:com-datou/config-file-example.git
+git clone git@github.com:com-datou/config-server-example.git
+git clone git@github.com:com-datou/eureka-client-example.git
+git clone git@github.com:com-datou/mqtt-client-example.git
